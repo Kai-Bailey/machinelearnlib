297c9643220915d50ff9b4bdb8fcc653921a28a3
Kai-Bailey <kai.bailey@icloud.com>
2018-05-05 12:31:27 -0600

    Setup python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='machinelearnlib',
+      version='0.1',
+      description='Implementation of common machine learning algorithms',
+      url='https://github.com/Kai-Bailey/machinelearnlib',
+      author='Kai Bailey',
+      author_email='kbailey1@ualberta.ca',
+      license='MIT',
+      packages=['machinelearnlib'],
+      zip_safe=False)
