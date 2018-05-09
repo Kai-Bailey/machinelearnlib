bfcea850a7c088262fa0ff9597ae92e754ea044b
Kai-Bailey <kai.bailey@icloud.com>
2018-05-08 21:36:16 -0600

    Setup unit testing

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,5 +1,12 @@
 import unittest
-from .config import machinelearnlib
+import os
+import sys
+
+# Insert the path to the machinelearnlib folder so python will search those modules on import statements
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import machinelearnlib.testing as testing
+
 
 class TestTesting(unittest.TestCase):
 
