6215d0d8f6559b350543aee365b22f1e5530587b
Kai-Bailey <kai.bailey@icloud.com>
2018-06-23 13:28:29 -0600

    Linear and Logistic Regression Prediction

--- machinelearnlib/models/linearRegression.py
+++ machinelearnlib/models/linearRegression.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def cost(features, labels, weights, regularization=0):
     """
@@ -25,10 +25,24 @@ def predict(features, weights):
            weights - Numpy column array of the learned weights of the model.
     """
     
-    pass
+    # Prepend a column of ones for bias feature
+    numEx = features.shape()[0]
+    biasFeature = np.ones((numEx,1))
+    features = np.concatenate((biasFeature, features))
 
-def gradent(features , labels, weights):
+    prediction = np.dot(features, weights)
+
+    return prediction
+
+
+def gradient(features , labels, weights):
     """
+    Computes the gradient of the cost function For linear regression. 
+
+    Param: features - Numpy matrix of input data used to make prediction.
+                      Each row is a training example and each feature is a column.
+    labels - Numpy column vector of the correct output for each training example.
+    weights - Numpy column vector of the learned weights of the model.
     """
     pass
 
--- machinelearnlib/models/logisticRegression.py
+++ machinelearnlib/models/logisticRegression.py
@@ -1,11 +1,51 @@
 import numpy as np
+from machinelearnlib import activationFunc
 
-a = np.array([4, 8, 12, 5])
-b = np.array([2, 4, 3, 2])
-c = a/b
+def cost(features, labels, weights, regularization=0):
+    """
+    Computes the squared error between the labels and predicted output with regularization for each weight.
 
-print(c)
+    Param: features - Numpy matrix of input data used to make prediction.
+                      Each row is a training example and each feature is a column.
+           labels - Numpy column vector of the correct output for each training example.
+           weights - Numpy column vector of the learned weights of the model.
+           regularization - Integer representing the regularization.
 
+    Return: Cost of the linear regression model.
+    """
+
+    pass
+
+
+def predict(features, weights):
+    """
+    Given the input data features and the weights computes the predictions from the linear regression model.
+
+    Param: features - Numpy matrix of input data used to make prediction.
+                      Each row is a training example and each feature is a column.
+           weights - Numpy column array of the learned weights of the model.
+    """
+    
+    # Prepend a column of ones for bias feature
+    numEx = features.shape()[0]
+    biasFeature = np.ones((numEx,1))
+    features = np.concatenate((biasFeature, features))
+
+    prediction = activationFunc.sigmoid(np.dot(features, weights))
+
+    return prediction
+
+def gradient(features , labels, weights):
+    """
+    Computes the gradient of the cost function For linear regression. 
+
+    Param: features - Numpy matrix of input data used to make prediction.
+                      Each row is a training example and each feature is a column.
+    labels - Numpy column vector of the correct output for each training example.
+    weights - Numpy column vector of the learned weights of the model.
+    """
+    
+    pass
 
 
 
