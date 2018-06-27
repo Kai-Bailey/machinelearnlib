75ab8d90e33ed255045429ffe068acf073e3b3ce
Kai-Bailey <kai.bailey@icloud.com>
2018-06-26 22:20:30 -0600

    Basic Load data for csv

---
+++ machinelearnlib/loadData.py
@@ -0,0 +1,21 @@
+from numpy import genfromtxt
+
+def loadData(mlModel):
+    """
+    Load in data from the dat folder using the file names specified in the mlModel
+    """
+    trainFileName = './data/' + mlModel['trainDataFileName']
+    testFileName = './data/' + mlModel['testDataFileName']
+    trainData = genfromtxt(trainFileName, delimiter=',')
+    testData = genfromtxt(testFileName, delimiter=',')
+
+    mlModel['features'] = trainData[:, :-1]
+    mlModel['labels'] = trainData[:, -1]
+
+    mlModel['testFeatures'] = testData[:, :-1]
+    mlModel['testLabels'] = testData[:, -1]
+
+
+
+
+
