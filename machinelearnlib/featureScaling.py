b2ffabc01b7e8374030350b7812cedb3426d798d
Kai-Bailey <kai.bailey@icloud.com>
2018-06-28 18:58:24 -0600

    Added Features Scaling

---
+++ machinelearnlib/featureScaling.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def meanNormalization(mlModel):
+    """
+    Subtract the mean from each feature from that feature so the data
+    is centered around 0.
+    """
+
+    features = mlModel['features']
+    numFeatures = features.shape[1]
+
+    means = []
+    for i in range(numFeatures):
+        mean = np.mean(features[:,i], dtype='float32')
+        features[:, i] = features[:, i] - mean
+        means.append(mean)
+
+    # Save to do predictions on real data later
+    mlModel['mean'] = np.asarray(means) 
+
+def stdNormalization(mlModel):
+    """
+    Divide each feature by the standard deviation for that feature. 
+    """
+
+    features = mlModel['features']
+    numFeatures = features.shape[1]
+
+    stds = []
+    for i in range(numFeatures):
+        std = np.std(features[:, i], dtype='float32')
+        features[:, i] = np.divide(features[:, i], std)
+        stds.append(std)
+
+    # Save std to do predictions on real data later
+    mlModel['std'] = np.asarray(stds)
+
+
+
+
+
+
+
---
+++ tests/test_featureScaling.py
@@ -0,0 +1,35 @@
+import unittest
+import os
+import sys
+import numpy as np
+
+# Insert the path to the machinelearnlib folder so python will search those modules on import statements
+topLevelFolder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+machinelearnlibFolder = os.path.join(topLevelFolder, 'machinelearnlib')
+sys.path.insert(0, machinelearnlibFolder)
+
+import featureScaling
+
+
+class TestFeaturesScaling(unittest.TestCase):
+        
+    def test_meanNormalization(self):
+
+        mlModel = {
+            'features':np.array([[2, 5, 4], [3, 6, 8], [9, 3, 9]], dtype='float32')
+        }
+
+        featureScaling.meanNormalization(mlModel)
+        np.testing.assert_allclose(mlModel['mean'], np.array([4.666666, 4.6666666, 7]), atol=0.001)
+        np.testing.assert_allclose(mlModel['features'], np.array([[-2.666666, 0.333333, -3.0], [-1.666666, 1.333333, 1.0], [4.3333, -1.666666, 2]]), atol=0.001)
+        
+    
+    def test_standardDeviationNormalization(self):
+
+        mlModel = {
+            'features':np.array([[2, 5, 4], [3, 6, 8], [9, 3, 9]], dtype='float32')
+        }
+
+        featureScaling.stdNormalization(mlModel)
+        np.testing.assert_allclose(mlModel['std'], np.array([3.0912, 1.247219, 2.160247]), atol=0.001)
+        np.testing.assert_allclose(mlModel['features'], np.array([[0.646997, 4.00819, 1.85164], [0.970495, 4.810702, 3.70328], [2.911485, 2.405351, 4.166191]]), atol=0.001)
