d72f0e8cb1cd8d0ff6cc62bac46c502cd7b1a97c
Kai-Bailey <kai.bailey@icloud.com>
2018-07-02 18:23:20 -0600

    Iris dataset and a function to load data set

--- README.md
+++ README.md
@@ -10,5 +10,8 @@ A Python Package of Common Machine Learning Algorithms
 
 ## Credit
 Example Data From:
+Linear Data Set
 https://www.kaggle.com/andonians/random-linear-regression
 
+Iris Data Set
+https://archive.ics.uci.edu/ml/datasets/iris
--- machinelearnlib/loadData.py
+++ machinelearnlib/loadData.py
@@ -1,19 +1,54 @@
+import numpy as np
+import pandas as pd
 from numpy import genfromtxt
 
 def loadData(mlModel):
     """
-    Load in data from the dat folder using the file names specified in the mlModel
+    Load in data from the data folder using the file names specified in teh mlModel
     """
-    trainFileName = './data/' + mlModel['trainDataFileName']
-    testFileName = './data/' + mlModel['testDataFileName']
-    trainData = genfromtxt(trainFileName, delimiter=',')
-    testData = genfromtxt(testFileName, delimiter=',')
+
+    if "trainDataFileName" in mlModel:
+        trainFileName = './data/' + mlModel['trainDataFileName']
+        trainData = pd.read_csv(trainFileName)
+        mlModel['features'] = np.array(trainData.iloc[:,:-1])
+        mlModel['labels'] = np.array(trainData.iloc[:,-1])
+
+    if "testDataFileName" in mlModel:
+        testFileName = './data/' + mlModel['testDataFileName']         
+        testData = pd.read_csv(testFileName)
+        mlModel['features'] = np.array(testData.iloc[:,:-1])
+        mlModel['labels'] = np.array(testData.iloc[:,-1])
 
 
-    mlModel['features'] = trainData[:, :-1]
-    mlModel['labels'] = trainData[:, -1]
+def loadIrisData(mlModel):
+    """
+    Load the Iris Data Set from the data folder into the mlModel
+    """
 
-    mlModel['testFeatures'] = testData[:, :-1]
-    mlModel['testLabels'] = testData[:, -1]
+    if "trainDataFileName" in mlModel:
+        trainFileName = './data/' + mlModel['trainDataFileName']
+        trainData = pd.read_csv(trainFileName)
+
+        print(trainData.head())
+
+        # Convert the labels 
+        unique = {}
+        uniqueID = 0
+        row = 0
+        for label in trainData.iloc[:,-1]:
+            if label not in unique:
+                unique[label] = uniqueID
+                uniqueID += 1
+
+            trainData.iloc[row, -1] = unique[trainData.iloc[row, -1]]
+            row += 1
+
+        
+        print(trainData.head())
+
+        mlModel['features'] = np.array(trainData.iloc[:,:-1])
+        mlModel['labels'] = np.array(trainData.iloc[:,-1])
+
+
 
 
