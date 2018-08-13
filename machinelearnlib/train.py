7f3e363e3a95b1d6ea75732fdb98030245dc17ff
Kai-Bailey <kai.bailey@icloud.com>
2018-08-13 17:24:51 -0600

    Implemented gradient descent for neural network.

--- machinelearnlib/train.py
+++ machinelearnlib/train.py
@@ -18,12 +18,77 @@ def gradientDescent(mlModel):
     mlModel.cost = cost
     return mlModel
 
-def backPropogation(mlModel):
+def gradientDescentNeuralNetwork(mlModel):
+    weights = mlModel.weights
+    features = mlModel.features
+    labels = mlModel.labels
+    testFeatures = mlModel.testFeatures
+    testLabels = mlModel.testLabels
+    reg = mlModel.regularization
+    learnRate = mlModel.learningRate
 
-    activation = mlModel.forwardPropagation(mlModel.features, mlModel.weights)
-    print(activation[-1][:][0])
+    testCost = [mlModel.cost(testFeatures, testLabels, weights, reg)]
+    cost = [mlModel.cost(features, labels, weights, reg)]
+    for i in range(mlModel.iterations):
+        grad = mlModel.backPropagation(features, labels, weights, reg)
+        
+        for j in range(len(weights)):
+            weights[j] -= learnRate * grad[j]
 
-    pred = mlModel.predict(mlModel.features, mlModel.weights)
-    print(pred)
+        currCost = mlModel.cost(features, labels, weights, reg)
+        cost.append(currCost)
+        currTestCost = mlModel.cost(testFeatures, testLabels, weights, reg)
+        testCost.append(currTestCost)
+        print("Iteration:{} | Cost:{} | TestCost: {}".format(i, cost[i], testCost[i]))
 
+    mlModel.testCost = testCost
+    mlModel.cost = cost
+    return mlModel
+
+def accuracyNeuralNetworkTest(mlModel):
+    features = mlModel.testFeatures
+    weights = mlModel.weights
+    labels = mlModel.testLabels
+
+    numEx = features.shape[0]
+    predictions = mlModel.predict(features, weights)
+
+    correct = 0
+    for i in range(len(predictions)):
+        if labels[predictions[i], i] == 1:
+            correct += 1
+
+    print("Test Set Accuracy: ", correct/numEx)
+    return mlModel
+
+def accuracyNeuralNetwork(mlModel):
+    features = mlModel.features
+    labels = mlModel.labels
+    testFeatures = mlModel.testFeatures
+    testLabels = mlModel.testLabels
+    weights = mlModel.weights
+
+    testNumEx = testFeatures.shape[0]
+    numEx = features.shape[0]
+    testPredictions = mlModel.predict(testFeatures, weights)
+
+    testCorrect = 0
+    for i in range(len(testPredictions)):
+        if testLabels[testPredictions[i], i] == 1:
+            testCorrect += 1
+
+    predictions = mlModel.predict(features, weights)
+
+    for i in range(10):
+        print("Prediction: ", predictions[i])
+        print("Label: ", labels[:,i])
+        plots.plotMNIST(features[i,:])
+
+    correct = 0
+    for i in range(len(predictions)):
+        if labels[predictions[i], i] == 1:
+            correct += 1
+
+    print("Accuracy: ", correct/numEx)
+    print("Test Set Accuracy: ", testCorrect/testNumEx)
     return mlModel
