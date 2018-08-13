11c82355afc741daa39d307c4a88fe855951798b
Kai-Bailey <kai.bailey@icloud.com>
2018-08-12 21:56:57 -0600

    Implemented backprop for neural network

--- machinelearnlib/activationFunc.py
+++ machinelearnlib/activationFunc.py
@@ -4,8 +4,18 @@ def sigmoid(arr):
     """
     Passes each element of the numpy array arr through the sigmiod function elementwise.
 
-    Param: arr - numpy array
-    Return: arr passed through sigmoid function elementwise
+    :param arr: numpy array
+    :return: arr passed through sigmoid function elementwise
     """
 
-    return np.exp(arr)/(np.exp(arr) + 1)
+    return 1/(1 + np.exp(-1 * arr))
+
+def sigmoid_gradient(arr):
+    """
+    Compute the gradient of the sigmoid function for each element in arr,
+
+    :param arr: numpy array
+    :return: arr passed through the gradient of the sigmoid function elementwise
+    """
+
+    return sigmoid(arr) * (1-sigmoid(arr))
--- machinelearnlib/models/neuralNet.py
+++ machinelearnlib/models/neuralNet.py
@@ -20,7 +20,8 @@ def cost(features, labels, weights, reg):
     cost = 0    
     numEx = features.shape[0]
 
-    hyp = forwardPropagation(features, weights)[-1]
+    _, activ = forwardPropagation(features, weights)
+    hyp = activ[-1]
 
     # Compute cost without regularization
     cost = (-labels * np.log(hyp)) - ((1-labels) * np.log(1-hyp))
@@ -32,18 +33,12 @@ def cost(features, labels, weights, reg):
     reg_cost = 0
     for weight in weights:
         test = np.sum(np.square(weight[:,1:]))
-        print("reg ",test)
         reg_cost += test
 
     reg_cost = (reg / (2*numEx)) * reg_cost
 
     return cost + reg_cost
 
-
-def gradient():
-    """
-    """
-    pass
 
 def forwardPropagation(features, weights):
     """
@@ -52,26 +47,35 @@ def forwardPropagation(features, weights):
     :param features: Numpy matrix of input data used to make prediction.
                       Each row is a training example and each feature is a column.
     :param weights: Numpy array of the learned weights of the model.
-    :returns: A list of numpy arrays containing the activation of the neural network for each layer except for
-              the input layer. Each column is a training example and each row is a node in the neural net.
+    :returns: Two lists of numpy arrays containing the intermidiate value (weighted sum before activation func) and the
+              activation of the neural network for each layer except for the input layer. Each column is a training
+              example and each row is a node in the neural net.
     """
     
+    # Only one example provided
+    if features.ndim == 1:
+        features = features.reshape(1, features.shape[0])
+
+
     num_ex = features.shape[0]
     bias_collumn = np.ones((num_ex, 1))
     activ = np.concatenate((bias_collumn, features), axis=1)
     activ = np.transpose(activ)
 
     activations = []
+    intermediates = []
     for weight in weights:
-        activ = np.dot(weight, activ)
-        activ = activationFunc.sigmoid(activ)
+        inter = np.dot(weight, activ)
+        intermediates.append(inter)
+        activ = activationFunc.sigmoid(inter)
         activations.append(activ)
 
         # Add column of ones for bias unit
         bias_row = np.ones((1,num_ex))
         activ = np.concatenate((bias_row, activ), axis=0)
 
-    return activations
+    return intermediates, activations
+
 
 def predict(features, weights):
     """
@@ -84,8 +88,61 @@ def predict(features, weights):
     :returns: The predicted value from the neural network for each training example.
     """
 
-    activations = forwardPropagation(features, weights)
+    _, activations = forwardPropagation(features, weights)
     return np.argmax(activations[-1],axis=0)
+
+
+def backPropagation(features, labels, weights, reg):
+    """
+    Compute the gradients of the
+
+    :param features:
+    :param weights:
+    :param labels:
+    :param reg:
+    :returns:
+    """
+    numEx = features.shape[0]
+    numFeat = features.shape[1]
+
+    # Build list of numpy arrays to store the cumulative gradient for each weight
+    # Gradient is not stored for the bias node 
+    accumGrad = []
+    for weight in weights:
+        accumGrad.append(np.zeros(weight.shape))
+
+    # Iterate through each training example
+    for i in range(numEx):
+        errors = []
+        intermediate, activations = forwardPropagation(features[i,:], weights)
+        errors.append(activations[-1] - labels[:,i].reshape(labels.shape[0], 1))
+
+        # Compute errors for each layer of nodes from right to left
+        for weight, inter in zip(reversed(weights[1:]), reversed(intermediate[:-1])):
+            err = np.dot(weight[:,1:].T, errors[-1]) * activationFunc.sigmoid_gradient(inter)
+            errors.append(err)
+
+        # Reverse errors so it nodes mach shape of neural net (left to right)
+        errors.reverse()        
+
+        # Calculate gradient for each weight in neural network
+
+        currFeature = np.hstack((1, features[i,:])).reshape(1, numFeat+1)
+
+        accumGrad[0] += np.dot(errors[0], currFeature)
+        for accum, error, activ in zip(accumGrad[1:], errors[1:], activations[:-1]):
+            bias_activ = np.vstack((np.array([1]).reshape((1,1)), activ))
+            accum += np.dot(error, bias_activ.T)
+
+
+    for i, weight in zip(range(len(accumGrad)), weights):
+        accumGrad[i] = (1/numEx) * accumGrad[i]
+
+        # Don't regularize bias term
+        accumGrad[i][:, 1:] += (reg/numEx) * weight[:,1:] 
+
+    return accumGrad
+
 
 
 def load(mlModel):
@@ -95,195 +152,6 @@ def load(mlModel):
     mlModel.predict = predict
     mlModel.forwardPropagation = forwardPropagation
     mlModel.backPropagation = backPropagation
-    mlModel.gradient = gradient
     mlModel.cost = cost
 
     loadModel.load("neuralNet")
-
-
-# def initializeWeights(dimensions, bound):
-#     '''
-#     Initialize the weights to uniformly dstributed random values from -bound to
-#     +bound.
-#     '''
-
-#     weights = []
-
-#     for i in range(0, dimensions.shape[0] - 1):
-#         weights.append(np.random.uniform(-bound, bound, (dimensions[i + 1],
-#                                                          dimensions[i] + 1)))
-
-#     return weights
-
-
-# def sigmoid(array):
-#     '''
-#     All elements of array will go through the sigmoid function.
-#     '''
-
-#     array = 1 / (1 + np.e ** (-array))
-#     return array
-
-
-# def forwardPropagation(feature, weights, dimension):
-#     '''
-#     Forward propagate through the neural newtork using the current weights and
-#     and biases. The activation of the nodes in the second to last layers will
-#     be returned.
-#     '''
-
-#     feature = np.insert(feature, 0, 1)
-
-#     activation = []   # List of np arrays containing activations for each node
-#     activation.append(sigmoid(np.matmul(weights[0], feature)))
-#     activation[0] = np.insert(activation[0], 0, 1)
-
-#     for i in range(1, dimensions.shape[0] - 1):
-#         activation.append(sigmoid(np.matmul(weights[i], activation[i - 1])))
-#         activation[i] = np.insert(activation[i], 0, 1)
-
-#     activation[-1] = np.delete(activation[-1], 0)
-
-#     return activation
-
-
-# def prediction(features, weights, dimensions):
-#     '''
-#     Forward propgate through the neural network using the current weights and
-#     biases. An array containing one hot vectors for each training example will
-#     be returned.
-#     '''
-
-#     predictions = np.empty([features.shape[0], dimensions[-1]], dtype=float)
-#     features = np.insert(features, 0, 1, axis=1)
-#     features = np.transpose(features)
-
-#     for i in range(0, features.shape[1]):
-#         prevAct = sigmoid(np.matmul(weights[0], features[:, i]))
-#         prevAct = np.insert(prevAct, 0, 1)
-
-#         for j in range(1, dimensions.shape[0] - 2):
-#             activations = sigmoid(np.matmul(weights[j], prevAct))
-#             prevAct = np.insert(activations, 0, 1)
-
-#         predictions[i, :] = sigmoid(np.matmul(weights[-1], prevAct))
-
-#     return predictions
-
-
-# def cost(features, weights, labels, dimensions, regularization):
-#     '''
-#     Returns the cost of the current weights and biases on the full training set
-#     '''
-
-#     predictions = prediction(features, weights, dimensions)
-
-#     cost = 0.0
-#     for i in range(0, features.shape[0]):
-#         for j in range(0, dimensions[-1]):
-#             if labels[i, j] == 0:
-#                 cost += np.log10(1 - predictions[i, j])
-#             else:
-#                 cost += np.log10(predictions[i, j])
-
-#     cost = -(1 / features.shape[0]) * cost
-
-#     regTerm = 0.0
-#     for i in range(0, len(weights)):
-#         regTerm += np.sum(weights[i] ** 2)
-
-#     regTerm = (regularization / (features.shape[0] * 2)) * regTerm
-#     cost += regTerm
-
-#     return cost
-
-
-# def gradCheck(features, weights, labels, dimensions, reg):
-#     '''
-#     gradCheck is used to verify that backPropagation is working correctly and
-#     should not be used in practice as performance is very slow. Verfifies that
-#     the gradient in backPropagation is correct by apprroximating the gradient.
-#     '''
-
-#     e = 0.0001
-#     approxGrad = []
-
-#     for l in range(0, len(weights)):
-#         approxGrad.append(np.empty_like(weights[l]))
-
-#         for j in range(weights[l].shape[0]):
-#             for i in range(weights[l].shape[1]):
-#                 currWeightsP = copy.deepcopy(weights)
-#                 currWeightsP[l][j][i] += e
-#                 currWeightsN = copy.deepcopy(weights)
-#                 currWeightsN[l][j][i] -= e
-#                 costP = cost(features, currWeightsP, labels, dimensions, reg)
-#                 costN = cost(features, currWeightsN, labels, dimensions, reg)
-#                 approxGrad[l][j][i] = (costP - costN) / (2.0 * e)
-
-#     print(approxGrad)
-
-
-# def backPropagation(features, weights, labels, dimensions, regularization,
-#                     learningRate):
-#     '''
-#     Computes back propagation through the neural network computing the gradient
-#     with respect to each weight and bias. The weights and biases are then
-#     changed accordingly and the new weights are returned.
-#     '''
-#     numLayers = dimensions.shape[0]
-#     numEx = features.shape[0]
-
-#     totError = []
-
-#     # Create list of error for each weight arranged like weights list
-#     for i in range(0, len(weights)):
-#         totError.append(np.empty_like(weights[i]))
-
-#     for i in range(0, numEx):
-#         error = []
-#         activation = forwardPropagation(features[i], weights, dimensions)
-
-#         error.insert(0, activation[-1] - labels[i, :])
-
-#         for j in range(numLayers - 2, 0, -1):
-#             temp = np.matmul(np.transpose(weights[j]), error[0])
-#             temp2 = np.multiply(temp, activation[j - 1])
-#             temp3 = np.multiply(temp2, (1 - activation[j - 1]))
-#             error.insert(0, temp3)
-
-#         totError[0] = np.outer(features[i], error[0])
-#         for j in range(1, numLayers - 1):
-#             totError[j] += np.matmul(activation[j], error[j])
-
-#     grad = []
-#     newWeights = []
-#     for i in range(0, numLayers - 1):
-#         grad.append((1 / numEx) + regularization * weights[i])
-#         newWeights.append(weights[i] - learningRate * grad[i])
-
-#     print(grad)
-#     return newWeights
-
-
-# def optimize(features, weights, labels, dimensions, reg, learningRate,
-#              iterations):
-#     '''
-#     Iterates through backPropagation and displays a graph plotting the cost
-#     function versus the number of iterations.
-#     '''
-
-#     costFunc = [cost(features, weights, labels, dimensions, reg)]
-#     for i in range(0, iterations):
-#         weights = backPropagation(features, weights, labels, dimensions, reg,
-#                                   learningRate)
-#         costFunc.append(cost(features, weights, labels, dimensions, reg))
-
-#     plt.figure()
-#     plt.title("Cost Function vs Iterations")
-#     plt.xlabel("Iterations")
-#     plt.ylabel("Cost")
-#     plt.plot(costFunc)
-#     plt.show()
-
-#     return weights
