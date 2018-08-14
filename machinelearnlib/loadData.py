bbf54d36322943b342afa0be513bbdbdce5d41ac
Kai-Bailey <kai.bailey@icloud.com>
2018-08-14 00:01:47 -0600

    Function to load data from Matlab

--- machinelearnlib/loadData.py
+++ machinelearnlib/loadData.py
@@ -1,14 +1,11 @@
 import numpy as np
 from numpy import genfromtxt
-import matplotlib.pyplot as plt
+import scipy.io 
 import pandas as pd
 
 def loadData(mlModel):
     """
     Load in data from the data folder using the file names specified in teh mlModel.
-
-    :param mlModel: Class defining the users options for the ml model.
-    :return mlModel: Class defining the users options for the ml model. 
     """
 
     if mlModel.trainDataFileName != None:
@@ -25,11 +22,43 @@ def loadData(mlModel):
 
     return mlModel
 
+def loadDataMatlab(mlModel):
+    """
+    Load Matlab data files. Note the matlab matrices must be named X, y for the training data
+    and the labels. The testing data must be named test_X and test_y.
+    """
+    if mlModel.trainDataFileName != None:
+        trainFileName = './data/' + mlModel.trainDataFileName
+        matlabData = scipy.io.loadmat(trainFileName)
+        mlModel.features = matlabData['X']
+        
+        # Convert labels to one hot
+        train_labels = matlabData['y'].T
+        train_labels = train_labels.T
+
+        onehot_train_labels = np.zeros((train_labels.size, 10))
+        onehot_train_labels[np.arange(train_labels.size), train_labels-1] = 1
+        mlModel.labels = onehot_train_labels.T
+
+    if mlModel.testDataFileName != None:
+        testFileName = './data/' + mlModel.testDataFileName
+        matlabData = scipy.io.loadmat(testFileName)
+        mlModel.testFeatures = matlabData['test_X']
+        mlModel.testLabels = matlabData['test_y']
+
+    return mlModel
+
+
+
 def loadDataMNIST(mlModel):
     """
-    Load the MNIST data set.
+    Load the MNIST data set. No training file names need to be specified but
+    mnist must be installed.
     """
-    import mnist
+    try:
+        import mnist
+    except:
+        raise("Please run pip install mnist")
 
     train_images = mnist.train_images()
     train_labels = mnist.train_labels()
@@ -42,23 +71,20 @@ def loadDataMNIST(mlModel):
     # Convert labels to one hot
     onehot_train_labels = np.zeros((train_labels.size, 10))
     onehot_train_labels[np.arange(train_labels.size), train_labels] = 1
-    mlModel.labels = onehot_train_labels
+    mlModel.labels = onehot_train_labels.T
 
     mlModel.testFeatures = test_images.reshape((test_images.shape[0], test_images.shape[1] * test_images.shape[2]))
 
     # Convert labels to one hot
     onehot_test_labels = np.zeros((test_labels.size, 10))
     onehot_test_labels[np.arange(test_labels.size), test_labels] = 1
-    mlModel.testLabels = onehot_test_labels
+    mlModel.testLabels = onehot_test_labels.T
 
     return mlModel
 
 def loadIrisData(mlModel):
     """
     Load the Iris Data Set from the data folder into the mlModel
-
-    :param mlModel: Class defining the users options for the ml model.
-    :return mlModel: Class defining the users options for the ml model. 
     """
 
     trainFileName = './data/' + mlModel.trainDataFileName
